7b34db363006e11bca671f794f2d37ca9772f7ca
devcarloshenrique <devcarloshenrique@gmail.com>
2020-06-22 22:10:59 -0300

    refactor: Layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,8 +6,9 @@ import ServerList from '../ServerList';
 import ServerName from '../ServerName';
 import ChannelInfo from '../ChannelInfo';
 import ChannelList from '../ChannelList';
-
-
+import UserInfo from '../UserInfo';
+import UserList from '../UserList';
+import ChannelData from '../ChannelData';
 
 
 const Layout: React.FC = () => {
@@ -17,6 +18,9 @@ const Layout: React.FC = () => {
             <ServerName />
             <ChannelInfo />
             <ChannelList />
+            <UserInfo />
+            <ChannelData />
+            <UserList />
         </Grid>
     )
 }
