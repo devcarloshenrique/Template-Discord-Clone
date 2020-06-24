18631392b0b27dfe3e19a6cad9dd957f63edf1a1
devcarloshenrique <devcarloshenrique@gmail.com>
2020-06-23 21:37:56 -0300

    refactor: loop implementation

--- src/components/ServerList/index.tsx
+++ src/components/ServerList/index.tsx
@@ -5,23 +5,25 @@ import ServerButton from '../ServerButton';
 import { Container, Separator }  from './styles';
 
 const ServerList: React.FC = () => {
+    const sorte = Math.random() >= 0.5;
     return (
         <Container>
             <ServerButton isHome/>
 
             <Separator/>
 
-            <ServerButton />
-            <ServerButton hasNotifications/>
-            <ServerButton mentions={3}/>
-            <ServerButton />
-            <ServerButton />
-            <ServerButton />
-            <ServerButton />
-            <ServerButton />
-            <ServerButton />
-            <ServerButton />
-            <ServerButton />
+            <ServerButton isImg={1}/>
+
+            {Array.from(Array(15).keys()).map((n) => (
+                <ServerButton       
+                isImg={n + 151}
+                hasNotifications={sorte} 
+                mentions={ (Math.random() >= 0.5)
+                    ? Math.floor(Math.random() * (99 - 0 + 1)) + 0
+                    : 0 
+                } 
+          />
+        ))}
         </Container> 
     )
 };
